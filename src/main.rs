727e95aa681810e3c46a7e9580534aad73f9df17
Massinissa Djellouli <massinissadjellouli@gmail.com>
2024-02-24 23:57:07 -0500

    build_image

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ pub extern "C" fn _start() -> ! {
 }
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> !{
-    print!("{}",_info);
+fn panic(info: &PanicInfo) -> !{
+    println!("{info}");
     loop{}
 }
