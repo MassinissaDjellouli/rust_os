4f40aece0fa4d2d029a76cd62d7bc0c45d26144b
Massinissa Djellouli <massinissadjellouli@gmail.com>
2024-03-05 23:10:10 -0500

    build_image

--- src/main.rs
+++ src/main.rs
@@ -13,10 +13,12 @@ use core::panic::PanicInfo;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    println!("Hello World{}", "!");
-
-    #[cfg(test)]
-    test_main();
+    #[cfg(not(test))]{
+        println!("Hello World{}", "!");
+    }
+    #[cfg(test)]{
+        test_main();
+    }
     loop {}
 }
 
