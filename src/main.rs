f65101dece53c068d728f90e805177dee4bf1397
Massinissa Djellouli <massinissadjellouli@gmail.com>
2024-03-05 23:07:52 -0500

    build_image

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,12 @@
 #![no_std]
 #![no_main]
-#![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
-#![reexport_test_harness_main = "test_main"]
-#[cfg(test)]
-fn test_runner(tests: &[&dyn Testable]){
-    serial_println!("Running {} tests",tests.len());
-    for test in tests{
-        test.run();
-    }
-    exit_qemu(Success);
-}
+
 
 mod vga_buf;
 mod serial;
+mod testing;
 
 use core::panic::PanicInfo;
-use crate::QemuExitCode::{Failed, Success};
 
 
 #[no_mangle]
@@ -34,45 +24,4 @@ fn panic(info: &PanicInfo) -> !{
     println!("{info}");
     loop{}
 }
-pub trait Testable {
-    fn run(&self) -> ();
-}
 
-impl<T> Testable for T
-    where
-        T: Fn(),
-{
-    fn run(&self) {
-        serial_print!("{}...\t", core::any::type_name::<T>());
-        self();
-        serial_println!("{}","[ok]");
-    }
-}
-
-#[panic_handler]
-#[cfg(test)]
-fn panic(info: &PanicInfo) -> !{
-    serial_println!("{}","[failed]");
-    serial_println!("Error: {}",info);
-    exit_qemu(Failed);
-    loop{}
-}
-#[test_case]
-fn trivial_assertion(){
-    assert_eq!(0,1);
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
-pub enum QemuExitCode{
-    Success = 0x10,
-    Failed = 0x11,
-}
-
-pub fn exit_qemu(exit_code: QemuExitCode){
-    use x86_64::instructions::port::Port;
-    unsafe{
-        let mut port = Port::new(0xf4);
-        port.write(exit_code as u32);
-    }
-}
